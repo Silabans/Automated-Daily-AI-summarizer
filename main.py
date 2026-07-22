fdc06826e2544e36c0c3870deb7c02417bf594ed
Matthew <matthew.yedija@gmail.com>
2026-07-22 22:29:40 +0800

    add: run logging into a separate after every run

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gen_api import summarise
 from discord_notifier import send_discord
+import logging
+import sys
 
 
 if __name__ == "__main__":
@@ -9,4 +11,15 @@ if __name__ == "__main__":
     for summary in partitioned:
         output = '\n\n\n' + summary + '\n\n\n'
         send_discord(output)
+
+    # Logging the last run made
+    logging.basicConfig(
+        filename='run_log.txt', # log into a file with this name (creation or overriding),
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    # Redirecting all print statements directly into the log file
+    sys.stdout = open(file='run_log.txt', mode='a') # standard print output
+    sys.stderr = open(file='run_log.txt', mode='a') # error output
 
